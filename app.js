e147fd0b64b58fdfaf13a6297d7ea24a78728f1b
krishnamaity <91984622+krishnamaity@users.noreply.github.com>
2023-09-08 15:39:20 +0000

    conversation number

---
+++ app.js
@@ -0,0 +1,18 @@
+const accountId=237654;
+let accountName="sbi";
+let accountState="west bengal";
+var accountnum="2340986";
+accountholder="krishna";
+// console.table([accountId,accountName,accountState,accountnum,accountholder]);
+
+// conversation of number//
+
+let number=33;
+let numbertoString=String(number);
+// console.log(numbertoString);
+// console.log(typeof(numbertoString));
+
+const bolean="hitesh";
+let booleantoNum=Boolean(bolean);
+console.log(booleantoNum);
+console.log(typeof booleantoNum);
