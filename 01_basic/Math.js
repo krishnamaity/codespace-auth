a14ec4718ff332cdb920c981bf3095cbf322349f
krishnamaity <91984622+krishnamaity@users.noreply.github.com>
2023-09-08 19:44:41 +0000

    date and time

---
+++ 01_basic/Math.js
@@ -0,0 +1,20 @@
+const balance = new Number(100);
+// console.log(balance);
+// console.log(Math);
+// console.log(Math.max(1,4,8));
+// console.log(Math.min(1,4,8));
+// console.log(Math.round(0.054));
+
+let max=20;
+let min=10;
+//console.log(Math.floor(Math.random()*10)+1);
+//console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+const date=new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+
+const event = new Date('August 19, 1975 23:15:30');
+console.log(event.toString());
+console.log(event.toLocaleString());
