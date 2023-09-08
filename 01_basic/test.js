05e5881276d15b2fc8f99d3622e2327de44f223d
krishnamaity <91984622+krishnamaity@users.noreply.github.com>
2023-09-08 18:08:57 +0000

    string methods

---
+++ 01_basic/test.js
@@ -0,0 +1,22 @@
+// prefix and postfix
+
+let x=3;
+let a=x++;
+//console.log(`value of a is ${a}`);
+
+// string methods
+
+const name="krishna maity";
+const age=20;
+//console.log(`my name is ${name} and age is ${age}`);
+const newName=new String("javascript");
+// console.log(newName);
+// console.log(__proto__);
+
+// console.log(`the character of string is ${newName.charAt(4)}`);
+// console.log(name.concat(newName));
+// console.log(name.replace("maity","gowsami"));
+// console.log(name)
+
+console.log(newName.slice(0,5));
+
